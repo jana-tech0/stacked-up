08b249289f492df00b148d3b9884de15e2b42949
jana tech <janatalari2020@gmail.com>
2025-04-05 12:24:45 +0530

    finish frontend UI and components

---
+++ src/pages/PlaceOrder/PlaceOrder.jsx
@@ -0,0 +1,58 @@
+import React, { useContext } from 'react'
+import './PlaceOrder.css'
+import { StoreContext } from '../../context/StoreContext'
+const PlaceOrder = () => {
+  const {getTotalCartAmount} = useContext(StoreContext)
+  return (
+   <form action="" className="place-order">
+    <div className="place-order-left">
+        <p className="title">Delivery Info</p>
+        <div className="multi-fields">
+        <input type="text" placeholder='First Name'/>
+        <input type="text"placeholder='Last Name' />
+        </div>
+        <input type="text" placeholder='Email'/>
+        <input type="text" placeholder='Street'/>
+        <div className="multi-fields">
+         <input type="text" placeholder='city'/>
+         <input type="text"placeholder='state' />
+       </div>
+       <div className="multi-fields">
+         <input type="text" placeholder='zip code'/>
+         <input type="text"placeholder='country' />
+       </div>
+       <input type="text" placeholder='phone'/>
+    </div>
+    <div className="place-order-right">
+    <div className="cart-total">
+          <div>
+          <h2>
+            Cart Totals
+          </h2>
+          <div className="cart-total-details">
+            <p>Subtotal</p>
+            <p>{getTotalCartAmount()}</p>
+          </div>
+          <hr />
+          <div className="cart-total-details">
+            <p>Delivery Fee</p>
+            <p>{getTotalCartAmount() === 0 ? 0: 2}</p>
+          </div>
+          <hr />
+          <div className="cart-total-details">
+            <b>
+              Total
+            </b>
+            <b>
+              {getTotalCartAmount() === 0 ? 0 : getTotalCartAmount() + 2}
+            </b>
+          </div>
+          <button >Proceed To Payments</button>
+        </div>
+    </div>
+    </div>
+   </form>
+  )
+}
+
+export default PlaceOrder
